78524b65a56d4aa9345b658dd68d3badebf59f72
Andre-Tobois <tobois.andre@gmail.com>
2019-01-02 22:09:45 -0500

    feat: Able to add book with author and isbn number.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Book Class: Represent a book
-class book{
+class Book{
   constructor(title, author, isbn){
     this.title = title;
     this.author = author;
@@ -41,7 +41,7 @@ class UI{
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
-    <td><a href="#" class="btn btn=danger btn-sm delete">X</a></td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
   
     list.appendChild(row);
@@ -54,5 +54,21 @@ class UI{
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Instatiate Book 
+  const book = new Book(title, author, isbn);
+  
+  //  Add books to UI
+  UI.addBookToList(book);
+  
+});
 
 // Event: Remove a Book
